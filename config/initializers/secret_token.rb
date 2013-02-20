893de4a47f8ec185d78d7d29b9ceeaf4ba65da43
amaharjan <amir.maharjan@gmail.com>
2013-02-20 18:23:18 +0545

    Added blog project to the git hub repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '853a08d3620bd88ae98290a3460c846b6710a2ed33799d5e183501b8cb8dc0be50952083751a939ffc21ba11c0a58ead40e512447e9d105d3fe6398a97413bbb'
